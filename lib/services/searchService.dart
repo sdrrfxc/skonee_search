9d7cf85d776727611931ad5e577938a6be836fef
sdrrfxc <kelby.criss@gmail.com>
2024-04-15 22:17:41 -0500

    Resolved errors from merge

--- lib/services/searchService.dart
+++ lib/services/searchService.dart
@@ -2,16 +2,18 @@ import 'dart:convert';
 import 'dart:core';
 import 'package:http/http.dart' as http;
 import 'package:skonee_search/models/resultModel.dart';
+import 'package:skonee_search/auth/secrets.dart';
+
 
 class SearchService {
-  static const _apiKey = 'BSAPBPOqLh9PEaHIlLH4proulQzM2pC';
   final httpClient = http.Client();
 
   Future<List<Result>> getResults(final searchTerm) async {
+    final apiKey =  secretKey;
     final requestUrl =
         'https://api.search.brave.com/res/v1/web/search?q=$searchTerm';
     final response = await httpClient
-        .get(Uri.parse(requestUrl), headers: {"X-Subscription-Token": _apiKey});
+        .get(Uri.parse(requestUrl), headers: {"X-Subscription-Token": apiKey});
 
     List<Result> returnResultList = [];
     final json = jsonDecode(response.body);
